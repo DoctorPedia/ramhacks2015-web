3ec81b895c94cdedcb3a67cdabf9d8e1bebdfef5
ShadowRonin <wunschelbs@vcu.edu>
2015-09-12 21:39:34 -0400

    Added Details to markers

--- mapExample/findDoctor.js
+++ mapExample/findDoctor.js
@@ -57,6 +57,12 @@ function populateMap () {
 				"<img src='"+doctorLocs[i].profile.image_url+"' height=20px widith=20px />" +
 
 			"</div>";
+			var infowindow = new google.maps.InfoWindow({
+			  content: contentString
+			});
+			marker.addListener('click', function() {
+			  infowindow.open(map, marker);
+			});
 		map.panTo(loc);
 	}
 }
